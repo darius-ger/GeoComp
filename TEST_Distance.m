252a0116690039a0943eb13d2a8179b2d5b58819
Matthias Wimmer <Matthias.Wimmer0@hm.edu>
2017-08-22 09:21:12 +0200

    modified the plot in TEST_Distance

--- TEST_Distance.m
+++ TEST_Distance.m
@@ -2,13 +2,25 @@
 % ipl_noise = geometry ('DeformedGeometry.stl');
 % ipl_noise.noise();
 % ipl_noise.translate('z',1);
-[pVError, nError] = ipl.distanceVertex2Geometry(ipl_noise);
+% [pVError, nError] = ipl.distanceVertex2Geometry(ipl_noise);
 
-%show
-figure
+% show
+figure;
 hold on;
-patch('Faces',ipl.Faces,'Vertices',ipl.Vertices,'EdgeColor','none','FaceVertexCData',pVError,'FaceColor','interp','FaceLighting','gouraud','AmbientStrength', 0.15);
-patch('Faces',ipl_noise.Faces,'Vertices',ipl_noise.Vertices,'FaceColor','none','FaceLighting','gouraud','AmbientStrength', 0.15);
+patch('Faces',ipl.Faces,...
+      'Vertices',ipl.Vertices,...
+      'EdgeColor','none',...
+      'FaceVertexCData',pVError,...
+      'FaceColor','interp',...
+      'FaceLighting','gouraud',...
+      'AmbientStrength', 0.15);
+patch('Faces',ipl_noise.Faces,...
+      'Vertices',ipl_noise.Vertices,...
+      'EdgeAlpha',0.5,...
+      'LineWidth',0.1,...
+      'FaceColor','none',...
+      'FaceLighting','gouraud',...
+      'AmbientStrength', 0.15);
 xlabel('x');
 ylabel('y');
 zlabel('z');
@@ -17,9 +29,17 @@ zlabel('z');
 camlight('headlight');
 material('dull');
           
-% Fix the axes scaling, and set a nice view angle
+% Fix the axes scaling, 
 axis('image');
+% rotate axis lables
+h = rotate3d;
+h.ActionPreCallback = @align_axislabel;
+h.ActionPostCallback= @align_axislabel;
+% set a nice view angle
 view([-135 35]);
-c = colorbar('east');
-c.Label.String = 'distance to target';
+% add and format a colorbar
+c = colorbar('eastoutside');
+c.AxisLocation='out';
+c.Label.String = 'distance to target [mm]';
+c.TickDirection='both';
 hold off;
